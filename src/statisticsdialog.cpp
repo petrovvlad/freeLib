223de8c00b437bd9c18e20d60f3ee4bb82a2baf2
petrovvlad <petrofff.vlad@gmail.com>
2024-06-03 21:15:36 +0300

    fix: количество авторов

--- src/statisticsdialog.cpp
+++ src/statisticsdialog.cpp
@@ -3,6 +3,7 @@
 #include "statisticsdialog.h"
 #include "ui_statisticsdialog.h"
 #include "library.h"
+#include "utilites.h"
 
 StatisticsDialog::StatisticsDialog(QWidget *parent) :
     QDialog(parent, Qt::Dialog | Qt::WindowTitleHint),
@@ -13,17 +14,17 @@ StatisticsDialog::StatisticsDialog(QWidget *parent) :
     QLocale locale;
     SLib& lib = libs[idCurrentLib];
     QString sText;
-    sText = QStringLiteral("<table>"
-                           "<tr><td width=\"50%\">%1</td><td>%2</td></tr>"
-                           "<tr><td>%3</td><td>%4</td></tr>"
-                           "<tr><td>%5</td><td>%6</td></tr>"
-                           "<tr><td>%7</td><td>%8</td></tr>"
-                           "<tr><td>%9</td><td>%10</td></tr>"
-                           "</table>").arg(
+    sText = u"<table>"
+             "<tr><td width=\"50%\">%1</td><td>%2</td></tr>"
+             "<tr><td>%3</td><td>%4</td></tr>"
+             "<tr><td>%5</td><td>%6</td></tr>"
+             "<tr><td>%7</td><td>%8</td></tr>"
+             "<tr><td>%9</td><td>%10</td></tr>"
+             "</table>"_s.arg(
                 tr("Library name"), lib.name,
                 tr("Version"), lib.sVersion,
                 tr("Book count"), locale.toString(lib.books.count()),
-                tr("Author count"), locale.toString(lib.authors.count())
+                tr("Author count"), locale.toString(lib.authors.count() - 1)
 #if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
                 ,
 #else
